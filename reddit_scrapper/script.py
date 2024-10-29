1f74acea87a9bdc6686c0d50c017022a3e251781
S-M-J-I <jishanlion@gmail.com>
2024-10-29 23:48:07 +0600

    modified limit

--- reddit_scrapper/script.py
+++ reddit_scrapper/script.py
@@ -15,47 +15,65 @@ reddit = praw.Reddit(
     user_agent=os.getenv("USER_AGENT"),
 )
 
+subreddits = [
+    "exmuslim",
+    "IsraelPalestine",
+    # "NoahGetTheBoat",
+    # "PoliticalCompassMemes",
+    "progressive_islam",
+    # "DebateReligion",
+    # "HolUp",
+    # "atheism",
+    # "religion",
+    # "PublicFreakout",
+    # "IndiaSpeaks",
+    # "AskReddit",
+    "AskMiddleEast",
+    "islam"
+]
+
 batches = 0
 itr = 0
-posts = reddit.subreddit("exmuslim").search(query="meme")
+base_limit = 100
 
 collections = {
     "image": [],
     "caption": []
 }
 
-for post in posts:
-    if batches % 10 == 0 and batches != 0:
-        print("Pause")
-        df = pd.DataFrame.from_dict(collections)
-        df.to_csv(f"chunk_{itr}.csv", index=False)
-        itr += 1
-        collections["image"].clear()
-        collections["caption"].clear()
-        time.sleep(10)
+for subreddit in subreddits:
+    posts = reddit.subreddit("exmuslim").search(query="meme", limit=base_limit)
 
-    print(post.title)
-    print(post.selftext)
-    if post.url.endswith(('.jpg', '.png', '.gif', 'jpeg')):
-        print(post.url)
-        image = requests.get(post.url)
-        with open(f"./reddit_memes/{post.id}.jpg", "wb") as file:
-            file.write(image.content)
+    for post in posts:
+        if batches % 10 == 0 and batches != 0:
+            print("Pause")
+            df = pd.DataFrame.from_dict(collections)
+            df.to_csv(f"chunk_{itr}.csv", index=False)
+            itr += 1
+            collections["image"].clear()
+            collections["caption"].clear()
+            time.sleep(3)
+
+        print(post.title)
+        print(post.selftext)
+        if post.url.endswith(('.jpg', '.png', '.gif', 'jpeg')):
+            print(post.url)
+            image = requests.get(post.url)
             caption = client.chat.completions.create(
                 model="llama-3.2-90b-vision-preview",
                 messages=[
                     {
                         "role": "user",
                         "content": [
+                                {
+                                    "type": "text",
+                                    "text": "Describe briefly how this meme represents Islam. Is it offensive to Islam?"
+                                },
                             {
-                                "type": "text",
-                                "text": "Describe how this meme represents Islam. Can it be considered offensive?"
-                            },
-                            {
-                                "type": "image_url",
-                                "image_url": {
-                                    "url": f"{post.url}"
-                                }
+                                    "type": "image_url",
+                                    "image_url": {
+                                        "url": f"{post.url}"
+                                    }
                             },
                         ]
                     },
@@ -64,7 +82,7 @@ for post in posts:
                         "content": ""
                     }
                 ],
-                temperature=1,
+                temperature=0,
                 max_tokens=1024,
                 top_p=1,
                 stream=False,
@@ -72,8 +90,13 @@ for post in posts:
             )
             content = caption.choices[0].message.content
             print(f"\033[31m{content}\033[0m")
-        print("\033[32mImage Saved\033[0m")
-        collections["image"].append(f"./reddit_memes/{post.id}.jpg")
-        collections["caption"].append(content)
-    print("========================")
-    batches += 1
+
+            # with open(f"./reddit_memes/{post.id}.jpg", "wb") as file:
+            #     file.write(image.content)
+            # print("\033[32mImage Saved\033[0m")
+
+            # collections["image"].append(f"./reddit_memes/{post.id}.jpg")
+            collections["image"].append(post.url)
+            collections["caption"].append(content)
+        print("========================")
+        batches += 1
