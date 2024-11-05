233d4e904bf3b4873a7d8bf63a9843e80b385b5d
sadia-ahmmed <sahmmed201146@bscse.uiu.ac.bd>
2024-11-05 23:02:14 +0600

    merged x-9gag-reddit into images

---
+++ unify_imagedir.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import requests
+import shutil
+import os
+
+# for file in os.listdir("./reddit_memes"):
+#     shutil.copyfile(f"./reddit_memes/{file}", f"./images/{file}")
+
+# for file in os.listdir("./x_9gag_scrapper/Image"):
+#     shutil.copyfile(f"./x_9gag_scrapper/Image/{file}", f"./images/{file}")
+
+
+print("DONE")
